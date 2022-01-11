73e7bd92e18fe3be20fb85da0192f34489538c58
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-01-11 02:21:18 +0100

    CHORE: added a modal attr

--- src/screens/viewFile/ViewFile.js
+++ src/screens/viewFile/ViewFile.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
 import {
   MaterialIcons,
   FontAwesome,
@@ -13,17 +21,18 @@ import {
 
 function ViewFile({ navigation, route }) {
   const { data, baseUrl, secretCode, serverSecret } = route.params;
-  console.log(data);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [tagname, setTagName] = useState(data.tag);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        {/* <EvilIcons
+        <EvilIcons
           name="close"
           size={24}
           style={styles.icon}
           onPress={() => navigation.goBack()}
-        /> */}
+        />
       </View>
 
       <View style={styles.imgContainer}>
@@ -32,7 +41,7 @@ function ViewFile({ navigation, route }) {
             ref={video}
             style={styles.video}
             source={{
-              uri: `${baseUrl}data/image?key=${data.fileName}&client_secret=${secretCode}&server_secret=${serverSecret}`,
+              uri: `${baseUrl}data/image?key=${tagname}&client_secret=${secretCode}&server_secret=${serverSecret}`,
             }}
             useNativeControls
             resizeMode="contain"
@@ -61,7 +70,7 @@ function ViewFile({ navigation, route }) {
         >
           <AntDesign name="clouddownload" size={45} color="#fff" />
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
           <Text style={styles.tag}>#{data.tag}</Text>
         </TouchableOpacity>
         <TouchableOpacity>
@@ -71,6 +80,38 @@ function ViewFile({ navigation, route }) {
           <MaterialIcons name="delete" size={40} color="#fff" />
         </TouchableOpacity>
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View>
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.modalText}
+              placeholder={data.tag}
+              style={styles.textInput}
+              onChangeText={(tagname) => setTagName(tagname)}
+              defaultValue={tagname}
+              placeholderTextColor="white"
+              color="white"
+            />
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                setTagName("New Name");
+              }}
+            >
+              <Text style={styles.tag}>Change Tagname</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -82,6 +123,8 @@ const styles = StyleSheet.create({
     flex: 1,
     height: "100%",
     backgroundColor: "#000",
+    position: "relative",
+    maxWidth: "100%",
   },
   header: {
     justifyContent: "center",
@@ -120,5 +163,32 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     paddingVertical: 12,
     borderRadius: 7,
+  },
+  modalView: {
+    position: "absolute",
+    top: 300,
+    left: 5,
+    width: "90%",
+    margin: 20,
+    backgroundColor: "#1D2026",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  textInput: {
+    marginVertical: 20,
+    borderWidth: 2,
+    borderColor: "#FF2465",
+    width: 200,
+    padding: 10,
+    borderRadius: 10,
   },
 });
